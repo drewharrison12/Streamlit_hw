be526a765d386849bfaa76931e6822d8ad9d1057
drewharrison12 <54385761+drewharrison12@users.noreply.github.com>
2024-04-10 10:46:58 -0400

    checking my filtering

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,8 @@ options = st.multiselect(
 
 
 st.write("### (3) show a line chart of sales for the selected items in (2)")
+
+st.dataframe(df[df['Category'] == option & df['Sub_Category'] == options])
+
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
 st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
