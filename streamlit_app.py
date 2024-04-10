a712c30a38bd0b0b7d3c593b5fe568240445fb57
drewharrison12 <54385761+drewharrison12@users.noreply.github.com>
2024-04-10 16:19:36 -0400

    columns?

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,6 +77,10 @@ margin = 1 - (sales - profit)/sales
 
 st.metric(label="Total Sales", value=sales)
 
+col1, col2, col3 = st.columns(3)
+col1.metric("Sales", sales)
+col2.metric("Profit", profit)
+col3.metric("Margin", margin)
 
 st.write('### Total Sales: $' + str(round(sales,2)))
 st.write('### Total Profit: $' + str(round(profit,2)))
